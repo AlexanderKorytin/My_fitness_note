497c903197a172df692e9a103bafa45d891208fd
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-07 23:28:17 +0300

    добавил splash экран

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation(libs.coroutines)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
---
+++ app/src/main/java/com/example/myfitnessnote/SplashActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myfitnessnote
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var showSplash: Job
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        showSplash = GlobalScope.launch {
+            delay(SHOW_TIME)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showSplash.cancel()
+    }
+
+    companion object {
+        private const val SHOW_TIME = 3000L
+    }
+}
