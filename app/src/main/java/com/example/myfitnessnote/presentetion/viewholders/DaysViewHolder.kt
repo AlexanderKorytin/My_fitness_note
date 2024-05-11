f90e8dc51421226e0ce733b7529c5b7fd9312db3
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-11 20:28:16 +0300

    исправил нумерацию дней

--- app/src/main/java/com/example/myfitnessnote/presentetion/viewholders/DaysViewHolder.kt
+++ app/src/main/java/com/example/myfitnessnote/presentetion/viewholders/DaysViewHolder.kt
@@ -6,7 +6,7 @@ import com.example.myfitnessnote.domain.models.DayItem
 
 class DaysViewHolder(private val rootBinding: DayItemBinding) : RecyclerView.ViewHolder(rootBinding.root){
     fun bind(dayItem: DayItem){
-        val dayName = "День ${dayItem.dayId}"
+        val dayName = "День ${dayItem.dayId+1}"
         val exerciseCounter = "${dayItem.exercisesCount} упражнений"
         rootBinding.tvDayCounter.text = exerciseCounter
         rootBinding.tvDayName.text = dayName
