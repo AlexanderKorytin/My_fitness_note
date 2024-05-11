df464890fee3a393aa075bc70b0f30c51ac104e3
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-11 20:16:48 +0300

    создал вьюмодель экрана списка дней

---
+++ app/src/main/java/com/example/myfitnessnote/presentetion/vievmodel/DaysViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.myfitnessnote.presentetion.vievmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfitnessnote.domain.api.DaysInteractor
+import com.example.myfitnessnote.presentetion.models.DaysIntent
+import com.example.myfitnessnote.presentetion.models.DaysScreenState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class DaysViewModel(private val interactor: DaysInteractor) : ViewModel() {
+
+    private val _daysScreenState: MediatorLiveData<DaysScreenState> =
+        MediatorLiveData(DaysScreenState.Loading)
+
+    val daysScreenState: LiveData<DaysScreenState> = _daysScreenState
+    fun update(intent: DaysIntent) {
+        when (intent) {
+            is DaysIntent.RequestDaysList -> {
+                viewModelScope.launch(Dispatchers.IO) {
+                    getDaysList()
+                }
+            }
+        }
+    }
+
+    private suspend fun getDaysList() {
+        try {
+            val list = interactor.getDayList()
+            _daysScreenState.postValue(DaysScreenState.Content(list))
+        } catch (e: Exception) {
+            _daysScreenState.postValue(DaysScreenState.Error)
+            delay(DELAY_REQUEST_LIST)
+            getDaysList()
+        }
+    }
+
+    companion object {
+        private const val DELAY_REQUEST_LIST = 1000L
+    }
+}
--- app/src/main/java/com/example/myfitnessnote/ui/fragment/DaysViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myfitnessnote.ui.fragment
-
-import androidx.lifecycle.ViewModel
-
-class DaysViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
