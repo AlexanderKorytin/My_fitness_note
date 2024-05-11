c020ba3e0467960fae81da32c97f98865e667e9c
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-11 22:19:31 +0300

    добавил зависимости

--- app/src/main/java/com/example/myfitnessnote/di/DataModule.kt
+++ app/src/main/java/com/example/myfitnessnote/di/DataModule.kt
@@ -1,12 +1,22 @@
 package com.example.myfitnessnote.di
 
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myfitnessnote.data.storage.api.DaysExercisesStorage
 import com.example.myfitnessnote.data.storage.impl.DaysExercisesStorageImpl
+import com.google.gson.Gson
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataModule = module {
+    single<SharedPreferences> {
+        androidContext().getSharedPreferences(APP_SETTINGS_PREF_KEY, AppCompatActivity.MODE_PRIVATE)
+    }
     single<DaysExercisesStorage> {
-        DaysExercisesStorageImpl(androidContext())
+        DaysExercisesStorageImpl(androidContext(), sharedPref = get(), json = get())
+    }
+    factory {
+        Gson()
     }
 }
+const val APP_SETTINGS_PREF_KEY = "App settings"
