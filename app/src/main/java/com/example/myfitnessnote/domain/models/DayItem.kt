7245899f94bcdcc221528ff0e544780742d1bb01
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-17 22:59:25 +0300

    изменил поля на список индексов тренировок

--- app/src/main/java/com/example/myfitnessnote/domain/models/DayItem.kt
+++ app/src/main/java/com/example/myfitnessnote/domain/models/DayItem.kt
@@ -2,6 +2,6 @@ package com.example.myfitnessnote.domain.models
 
 data class DayItem(
     val dayId: Int,
-    val exercisesCount: Int,
+    val exercisesIndexes: List<Int>,
     val isComplete: Boolean = false
 )
