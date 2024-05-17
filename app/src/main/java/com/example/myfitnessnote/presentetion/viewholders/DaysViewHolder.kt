13b1228d0c9d6aa90a2213e996d9786d8a5a2760
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-17 23:02:18 +0300

    получаем список вместо размера

--- app/src/main/java/com/example/myfitnessnote/presentetion/viewholders/DaysViewHolder.kt
+++ app/src/main/java/com/example/myfitnessnote/presentetion/viewholders/DaysViewHolder.kt
@@ -1,6 +1,5 @@
 package com.example.myfitnessnote.presentetion.viewholders
 
-import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myfitnessnote.R
 import com.example.myfitnessnote.databinding.DayItemBinding
@@ -9,7 +8,7 @@ import com.example.myfitnessnote.domain.models.DayItem
 class DaysViewHolder(private val rootBinding: DayItemBinding) : RecyclerView.ViewHolder(rootBinding.root){
     fun bind(dayItem: DayItem) = with(rootBinding){
         val dayName = "${root.context.getString(R.string.day)} ${dayItem.dayId+1}"
-        val exerciseCounter = "${dayItem.exercisesCount} ${root.context.getString(R.string.exercite)}"
+        val exerciseCounter = "${dayItem.exercisesIndexes.size} ${root.context.getString(R.string.exercite)}"
         tvDayCounter.text = exerciseCounter
         tvDayName.text = dayName
     }
