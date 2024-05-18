07edda440bc9e9eb976d835b62e420ad3b2cab91
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-19 00:16:36 +0300

    обрабатываю исключения

--- app/src/main/java/com/example/myfitnessnote/domain/impl/DaysInteractorImpl.kt
+++ app/src/main/java/com/example/myfitnessnote/domain/impl/DaysInteractorImpl.kt
@@ -5,22 +5,27 @@ import com.example.myfitnessnote.domain.api.DaysRepository
 import com.example.myfitnessnote.domain.models.DayItem
 import com.example.myfitnessnote.domain.models.ExerciseItem
 import com.example.myfitnessnote.domain.models.ExercisesListResult
+import java.lang.IndexOutOfBoundsException
 
 class DaysInteractorImpl(private val repository: DaysRepository) : DaysInteractor {
-    private var listDays: List<DayItem> = emptyList()
     override fun getDayList(): List<DayItem> {
-        listDays = repository.getDayList()
-        return listDays
+       return repository.getDayList()
     }
 
     override fun getDayExercises(dayId: Int): ExercisesListResult {
+        val listDays = getDayList()
         val data = repository.getAllExercises().getOrNull()
         val error = repository.getAllExercises().exceptionOrNull()
         val result = when {
             data != null -> {
                 val listDayExercises = mutableListOf<ExerciseItem>()
                 listDays[dayId].exercisesIndexes.forEach {
-                    listDayExercises.add(data[it])
+                    val currentExercise = try {
+                        data[it]
+                    } catch (e: IndexOutOfBoundsException) {
+                        data[0]
+                    }
+                    listDayExercises.add(currentExercise)
                 }
                 ExercisesListResult.Content(listDayExercises)
             }
