7aff285af8fcf58b493608e91d2b7e3684615527
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-19 00:23:40 +0300

    изменил пакет

--- app/src/main/java/com/example/myfitnessnote/presentetion/viewholders/DaysViewHolder.kt
+++ app/src/main/java/com/example/myfitnessnote/presentetion/viewholders/DaysViewHolder.kt
@@ -5,10 +5,12 @@ import com.example.myfitnessnote.R
 import com.example.myfitnessnote.databinding.DayItemBinding
 import com.example.myfitnessnote.domain.models.DayItem
 
-class DaysViewHolder(private val rootBinding: DayItemBinding) : RecyclerView.ViewHolder(rootBinding.root){
-    fun bind(dayItem: DayItem) = with(rootBinding){
-        val dayName = "${root.context.getString(R.string.day)} ${dayItem.dayId+1}"
-        val exerciseCounter = "${dayItem.exercisesIndexes.size} ${root.context.getString(R.string.exercite)}"
+class DaysViewHolder(private val rootBinding: DayItemBinding) :
+    RecyclerView.ViewHolder(rootBinding.root) {
+    fun bind(dayItem: DayItem) = with(rootBinding) {
+        val dayName = "${root.context.getString(R.string.day)} ${dayItem.dayId + 1}"
+        val exerciseCounter =
+            "${dayItem.exercisesIndexes.size} ${root.context.getString(R.string.exercise)}"
         tvDayCounter.text = exerciseCounter
         tvDayName.text = dayName
     }
