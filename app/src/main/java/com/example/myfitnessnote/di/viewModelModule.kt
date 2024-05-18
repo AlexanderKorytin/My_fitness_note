d3fb2fc1ae5171d91e88177b893e005ee0151a77
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-19 00:23:05 +0300

    добавил вью модель экрана списка упражнений в di

--- app/src/main/java/com/example/myfitnessnote/di/viewModelModule.kt
+++ app/src/main/java/com/example/myfitnessnote/di/viewModelModule.kt
@@ -1,11 +1,16 @@
 package com.example.myfitnessnote.di
 
 import com.example.myfitnessnote.presentetion.vievmodel.DaysViewModel
+import com.example.myfitnessnote.presentetion.vievmodel.ExerciseViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel {
         DaysViewModel(interactor = get())
+    }
+
+    viewModel {(dayId: Int) ->
+        ExerciseViewModel(interactor = get(), dayId = dayId)
     }
 }
