282e77afb00ffbb4b76fc506cf7e9aacac645ab7
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-22 23:52:50 +0300

    устранил ошибку

--- app/src/main/java/com/example/myfitnessnote/utils/BindingFragment.kt
+++ app/src/main/java/com/example/myfitnessnote/utils/BindingFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.example.myfitnessnote.presentetion.models.DaysScreenState
 
 abstract class BindingFragment<T : ViewBinding> : Fragment() {
 
