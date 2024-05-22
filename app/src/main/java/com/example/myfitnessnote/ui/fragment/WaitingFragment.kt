5946d93e82723acf4ec22b8c3479d3fa9de48ee1
AlexandrKorytin <al.corytin@yandex.ru>
2024-05-23 00:09:57 +0300

    вынес логику прогресса в отдельную функцию для дальнейшего использования

--- app/src/main/java/com/example/myfitnessnote/ui/fragment/WaitingFragment.kt
+++ app/src/main/java/com/example/myfitnessnote/ui/fragment/WaitingFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.myfitnessnote.databinding.FragmentWaitingBinding
 import com.example.myfitnessnote.utils.BindingFragment
+import com.example.myfitnessnote.utils.showExerciseProgress
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -29,33 +31,12 @@ class WaitingFragment : BindingFragment<FragmentWaitingBinding>() {
     }
 
     private fun bind() = with(binding) {
-        tvWaitProgress.max = COUNTER_START_VALUE
-        var progressCurrentValue = COUNTER_START_VALUE
-        val processStep = PROGRESS_DELAY.toInt()
-        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-            val counterJob = lifecycleScope.launch {
-                for (i in COUNTER_START_VALUE downTo COUNTER_END_VALUE step COUNTER_STEP) {
-                    tvWaitCounter.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(i)
-                    delay(COUNTER_DELAY)
-                }
-            }
-            val progressJob = lifecycleScope.launch(Dispatchers.IO) {
-                while (progressCurrentValue >= 0) {
-                    delay(PROGRESS_DELAY)
-                    progressCurrentValue -= processStep
-                    tvWaitProgress.progress = progressCurrentValue
-                }
-            }
-            progressJob.join()
-            TODO("navigateTo next fragment")
-        }
+       showExerciseProgress(COUNTER_START_VALUE, lifecycleScope, tvWaitCounter, tvWaitProgress){
+           findNavController().navigateUp()
+       }
     }
 
     companion object {
-        private const val COUNTER_DELAY = 1000L
-        private const val PROGRESS_DELAY = 100L
         private const val COUNTER_START_VALUE = 5000
-        private const val COUNTER_END_VALUE = 0
-        private const val COUNTER_STEP = 1000
     }
 }
--- app/src/main/java/com/example/myfitnessnote/utils/utils.kt
+++ app/src/main/java/com/example/myfitnessnote/utils/utils.kt
@@ -1,9 +1,14 @@
 package com.example.myfitnessnote.utils
 
+import android.widget.ProgressBar
+import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
 
 fun <T> debounce(
     delayMillis: Long,
@@ -24,3 +29,39 @@ fun <T> debounce(
         }
     }
 }
+
+fun showExerciseProgress(
+    time: Int,
+    scope: CoroutineScope,
+    tvCounter: TextView,
+    tvProcess: ProgressBar,
+    action: () -> Unit
+) {
+    tvProcess.max = time
+    var progressCurrentValue =
+        time
+    val processStep = 100
+    scope.launch(Dispatchers.IO) {
+        val counterJob = scope.launch {
+            for (i in time downTo 0 step COUNTER_STEP) {
+                tvCounter.text = SimpleDateFormat("mm:ss", java.util.Locale.getDefault()).format(i)
+                delay(COUNTER_DELAY)
+            }
+        }
+        val progressJob = scope.launch(Dispatchers.IO) {
+            while (progressCurrentValue >= 0) {
+                delay(PROGRESS_DELAY)
+                progressCurrentValue -= processStep
+                tvProcess.progress = progressCurrentValue
+            }
+        }
+        progressJob.join()
+        withContext(Dispatchers.Main) {
+            action.invoke()
+        }
+    }
+}
+
+private const val COUNTER_DELAY = 1000L
+private const val PROGRESS_DELAY = 100L
+private const val COUNTER_STEP = 1000
