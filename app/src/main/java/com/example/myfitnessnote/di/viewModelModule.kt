2d5a373c7de31e25d72e985e8ba8835b3a6c613a
AlexandrKorytin <al.corytin@yandex.ru>
2024-07-04 23:58:55 +0300

    добавил зависимость в di

--- app/src/main/java/com/example/myfitnessnote/di/viewModelModule.kt
+++ app/src/main/java/com/example/myfitnessnote/di/viewModelModule.kt
@@ -3,6 +3,7 @@ package com.example.myfitnessnote.di
 import com.example.myfitnessnote.presentetion.viewmodel.DayFinalViewModel
 import com.example.myfitnessnote.presentetion.viewmodel.DaysViewModel
 import com.example.myfitnessnote.presentetion.viewmodel.ExerciseViewModel
+import com.example.myfitnessnote.presentetion.viewmodel.TrainingViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -17,5 +18,8 @@ val viewModelModule = module {
 
     viewModel { (dayId: Int) ->
         DayFinalViewModel(interactor = get(), dayId = dayId)
+    }
+    viewModel {
+        TrainingViewModel(interactor = get())
     }
 }
