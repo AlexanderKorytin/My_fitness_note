c7b968f87a3a10fafb8572c4c6c504b84fe5b54f
AlexandrKorytin <al.corytin@yandex.ru>
2024-08-13 22:19:57 +0300

    добавил метод получения всех дней для первоначальной настройки

--- app/src/main/java/com/example/myfitnessnote/data/db/dao/DaysDao.kt
+++ app/src/main/java/com/example/myfitnessnote/data/db/dao/DaysDao.kt
@@ -16,6 +16,9 @@ interface DaysDao {
     @Query("SELECT * FROM ${DayItemDto.TABLE_NAME_DAYS_LIST} WHERE difficulty = :difficulty")
     fun getDaysByDifficulty(difficulty: Difficulty): Flow<List<DayItemDto>>
 
+    @Query("SELECT * FROM ${DayItemDto.TABLE_NAME_DAYS_LIST}")
+    fun getDaysByDifficulty(): Flow<List<DayItemDto>>
+
     @Query("SELECT * FROM ${DayItemDto.TABLE_NAME_DAYS_LIST} WHERE dayId = :dayId")
     fun getDayById(dayId: Int): Flow<DayItemDto>
 }
